5daae1167be2d68b1bf09566ff3bb62e7d10364e
omarakamal <omaraakamal@gmail.com>
2026-02-02 19:36:08 +0300

    update

--- intro-to-react/src/App.jsx
+++ intro-to-react/src/App.jsx
@@ -1,6 +1,8 @@
+import Navbar from './components/Navbar'
 function App() {
   return (
     <div>
+      <Navbar></Navbar>
       <h1>Welcome to my Website</h1>
     </div>
   )
---
+++ intro-to-react/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar(){
+    return(
+        <div>
+            <p>Homepage</p>
+            <p>About</p>
+            <p>Donate</p>
+        </div>
+    )
+}
+
+export default Navbar
